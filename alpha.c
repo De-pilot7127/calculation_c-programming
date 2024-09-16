5ea61a628c48e24797a83e419dff4a8a2e404b85
De-pilot7127 <stephenonesmus19@gmail.com>
2024-09-16 10:45:09 +0300

    First valid commit

---
+++ alpha.c
@@ -0,0 +1,18 @@
+//programming two digits
+#include<stdio.h>
+int main()
+{
+
+int l,w,mutiply;
+printf("enter the frist number");
+scanf("%d",&l);
+printf("enter the second number");
+scanf("%d",&w);
+
+mutiply=l*w;
+
+printf("the product of l&w is %d",mutiply);
+
+
+
+}
---
+++ sum -project -0.1.c
@@ -0,0 +1,13 @@
+//programming two digits//
+#include<stdio.h>
+int main()
+{
+    int a,b ,sum;
+    printf("enter the  first number");
+    scanf("%d",&a);
+    printf("enter the  second number");
+    scanf("%d",&b);
+    sum= a+b;
+    printf("the sum of a and b is %d",sum);
+
+}
